46650d2ff837b7e9d356354d35cd31ba0d19ee93
alatysavage254 <briansumukwo23@gmail.com>
2025-03-26 10:33:09 +0300

    ass

---
+++ func.py
@@ -0,0 +1,10 @@
+def is_divisible_by_ten(num):
+    if num % 10 == 0: 
+        return True
+    return False  
+
+
+print(is_divisible_by_ten(50))   
+print(is_divisible_by_ten(23))   
+print(is_divisible_by_ten(100))  
+print(is_divisible_by_ten(7))    
